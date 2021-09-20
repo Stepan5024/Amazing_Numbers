15d9679eb4ff0264f97353ac5d2d5310e881701a
Степан <bokarevstepan1@yandex.ru>
2021-09-20 14:55:34 +0300

    Добавил определение нуля в числе (duck свойство)

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -5,45 +5,47 @@ import java.util.Scanner;
 import static java.lang.System.exit;
 
 public class Main {
+    static Boolean isNatural = false;
+    static Boolean isEven = false;
+    static Boolean isOdd = false;
+    static Boolean isBuzz = false;
+    static Boolean isDuck = false;
+
     public static void main(String[] args) {
 //        write your code here
         System.out.println("Enter a natural number:");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        if (number <= 0) {
-            System.out.println("This number is not natural!");
-            exit(0);
-        }
+        checkIfNatural(number);
+
         System.out.println("Properties of " + number);
-        if (number % 2 == 1) System.out.println("\todd: true\n\teven: false");
-        else System.out.println("\todd: false\n\teven: true");
-        int temp = number;
-        boolean seven = false;
 
-        int lastDigit = temp % 10;
-        temp /= 10;
-        lastDigit *= 2;
-        int res = temp - lastDigit;
-        if (res % 7 == 0) {
-            seven = true;
+        checkIfOddOrEven(number);
+        System.out.println("\todd: " + isOdd + "\n\teven: " + isEven);
+
+        ;
+        System.out.println("\tbuzz: " + checkIfBuzz(number));
+        System.out.println("\tduck: " + checkIfDuck(number));
+
+
+    }
+    static void checkIfOddOrEven(int userNumber) {
+        isEven = (userNumber % 2 == 0) ? true : false;
+        isOdd = (userNumber % 2 != 0) ? true : false;
+    }
+
+    static void checkIfNatural(int userNumber) {
+        isNatural = userNumber > 0 ? true : false;
+
+        if (!isNatural) {
+            System.out.println("This number is not natural!");
+            System.exit(0);
         }
-
-        if (number % 10 != 7 && !seven) {
-            System.out.println(number + " is neither divisible by 7 nor does it end with 7");
-        }else if(seven && number % 10 != 7) {
-            System.out.println("It is a Buzz number.\n" +
-                    "Explanation:");
-            System.out.println(number + " is divisible by 7");
-        } else if(!seven && number % 10 == 7) {
-            System.out.println("It is a Buzz number.\n" +
-                    "Explanation:");
-            System.out.println(number + " ends with 7");
-        }else{
-            System.out.println("It is a Buzz number.\n" +
-                    "Explanation:");
-            System.out.println(number + " is divisible by 7 and ends with 7");
-
-        }
-
+    }
+    static boolean checkIfDuck(int userNumber) {
+        return isDuck = ("" + userNumber).contains("0") && (""+userNumber).charAt(0) != '0' ? true : false;
+    }
+    static  boolean checkIfBuzz(int userNumber) {
+        return isBuzz = userNumber % 7 == 0 || userNumber % 10 == 7 ? true : false;
     }
 }
